71ba628fdc8cd3a220b8c42100a43947e810856f
denispimenov <pimenovdenis.prog@gmail.com>
2020-05-04 19:18:35 +0300

    Fix the readme converting

--- src/LeetSharpTool/ReadmeHelper.cs
+++ src/LeetSharpTool/ReadmeHelper.cs
@@ -8,7 +8,8 @@ namespace LeetSharpTool
         public static string ConvertToMd(string url, string html)
         {
             var sb = new StringBuilder();
-            sb.AppendLine("#Problem");
+            sb.AppendLine("# Problem");
+            sb.AppendLine();
             sb.AppendLine($"[Source]({url})");
             var converter = new Converter();
             sb.Append(converter.Convert(html));
